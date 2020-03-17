56d15b22b10c2c7fd6db4a7eed15b00bcdcc648a
Kuro Hsu <kurotanshi@gmail.com>
2020-03-17 17:11:18 +0800

    fix: option 重複定義

--- src/components/BaseVideoPlayer.vue
+++ src/components/BaseVideoPlayer.vue
@@ -21,25 +21,25 @@ export default {
     options: {
       type: Object,
       default() {
-        return {}
+
+        return {
+          autoplay: false,
+          controls: true,
+          poster: "",
+          sources: [
+            {
+              src:
+                "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+              type: "application/x-mpegURL"
+            }
+          ]
+        };
       }
     }
   },
   data() {
     return {
-      player: null,
-      options: {
-        autoplay: false,
-        controls: true,
-        poster: "",
-        sources: [
-          {
-            src:
-              "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
-            type: "application/x-mpegURL"
-          }
-        ]
-      }
+      player: null
     }
   },
   mounted() {
